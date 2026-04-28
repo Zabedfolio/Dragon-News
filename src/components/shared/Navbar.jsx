8323bd10b7a72a40c38355de218bc826a13363d3
Zabedfolio <zabedfolio@gmail.com>
2026-04-28 18:59:06 +0600

    added navbar with dynamic route

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,9 +1,35 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+import user from '@/assets/user.png'
+import NavLink from '@/components/shared/NavLink';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>Navbar</h2>
+        <div className='flex justify-between container mx-auto mt-6 mb-19'>
+            <div></div>
+
+            <ul className='flex gap-5 justify-between items-center text-[#706F6F]'>
+                <li>
+                    <NavLink href='/'>Home</NavLink>
+                </li>
+
+                <li>
+                    <NavLink href='/about-us'>About</NavLink>
+                </li>
+
+                <li>
+                    <NavLink href='/career'>Career</NavLink>
+                </li>
+            </ul>
+
+            <div className='flex gap-5 items-center'>
+                <Image alt='user' src={user} width={40} height={40}></Image>
+                <button className='btn btn-neutral px-8'>
+                    <Link href={'/login'}>Login</Link>
+                </button>
+            </div>
+
         </div>
     );
 };
